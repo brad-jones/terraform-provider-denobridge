4339622602fa9a2997e7d717b7dd964ebe7f421d
Brad Jones <brad@bjc.id.au>
2026-01-15 17:13:03 +1100

    chore: make sure goreleaser can inject the version number

--- main.go
+++ main.go
@@ -8,8 +8,17 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
 
+var (
+	// these will be set by the goreleaser configuration
+	// to appropriate values for the compiled binary.
+	version string = "dev"
+
+	// goreleaser can pass other information to the main package, such as the specific commit
+	// https://goreleaser.com/cookbooks/using-main.version/
+)
+
 func main() {
-	err := providerserver.Serve(context.Background(), provider.New("0.0.0"), providerserver.ServeOpts{
+	err := providerserver.Serve(context.Background(), provider.New(version), providerserver.ServeOpts{
 		Address: "example.registry.local/brad-jones/denobridge",
 	})
 	if err != nil {
